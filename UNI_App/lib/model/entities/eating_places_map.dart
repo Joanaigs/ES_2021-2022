cdafc79ea029f42a67530cb63b2c75c19ac52b5c
inescardos <ines.caardoso@gmail.com>
2022-04-27 23:16:36 +0100

    Created the page of the map with all eating places

---
+++ UNI_App/lib/model/entities/eating_places_map.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class EatingPlacesMap extends StatelessWidget {
+  const EatingPlacesMap({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
