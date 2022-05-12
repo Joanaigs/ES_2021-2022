66415519df754060d80ab57378cb27a9df06299b
Joana <up202006279@edu.fe.up.pt>
2022-05-12 16:36:40 +0100

    added restaurant widget test

---
+++ UNI_App/test/unit/view/Widgets/eatAtFeup/eating_place_card_test.dart
@@ -0,0 +1,23 @@
+import 'package:uni/model/entities/exam.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:uni/view/Widgets/eatAtFeup/eating_place_card.dart';
+
+import '../../../../testable_widget.dart';
+
+void main() {
+  group('EatingPageCard', () {
+    String photo="ae.jpg";
+    String name="AE";
+    testWidgets('When given an restaurant', (WidgetTester tester) async {
+      final widget = makeTestableWidget(
+          child: BuildRestaurantCard(photoName: photo, name: name));
+      await tester.pumpWidget(widget);
+
+      expect(find.byType(Container), findsOneWidget);
+    });
+
+
+
+  });
+}
