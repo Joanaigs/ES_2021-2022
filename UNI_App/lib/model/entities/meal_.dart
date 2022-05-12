7a51e2de3842d9da5861baf5fc515bd90fbe93e1
Joana <up202006279@edu.fe.up.pt>
2022-05-12 21:29:38 +0100

    fixed file

--- UNI_App/lib/model/entities/meal_.dart
+++ UNI_App/lib/model/entities/meal_.dart
@@ -13,72 +13,70 @@ enum FoodType{
   Outro
 }
 
-Image getFoodTypeIcon(FoodType type){
-
-  Image getFoodTypeIcon(FoodType type) {
-    return Image.asset('assets/images/' + foodTypeToString(type) + '.png');
+Image getFoodTypeIcon(FoodType type) {
+  return Image.asset('assets/images/' + foodTypeToString(type) + '.png');
+}
+FoodType parseFoodType(String str){
+  str = str.replaceAll(' ', '').replaceAll('\n', '').replaceAll('\r', '').toLowerCase();
+  if(str == 'carne') {
+    return FoodType.Carne;
+  } else if (str == 'vegetariano') {
+    return FoodType.Vegetariano;
+  } else if (str == 'dieta') {
+    return FoodType.Dieta;
+  } else if (str == 'peixe') {
+    return FoodType.Peixe;
+  }else if(str == 'outro') {
+    return FoodType.Outro;
+  }else if (str == 'bebida') {
+    return FoodType.Bebida;
   }
-  FoodType parseFoodType(String str){
-    str = str.replaceAll(' ', '').replaceAll('\n', '').replaceAll('\r', '').toLowerCase();
-    if(str == 'carne') {
-      return FoodType.Carne;
-    } else if (str == 'vegetariano') {
-      return FoodType.Vegetariano;
-    } else if (str == 'dieta') {
-      return FoodType.Dieta;
-    } else if (str == 'peixe') {
-      return FoodType.Peixe;
-    }else if(str == 'outro') {
-      return FoodType.Outro;
-    }else if (str == 'bebida') {
-      return FoodType.Bebida;
-    }
-    else {
-      print(str);
-      return null;
-    }
-  }
-
-  String foodTypeToString(FoodType type) {
-    switch (type) {
-      case FoodType.Carne:
-        return 'Carne';
-      case FoodType.Peixe:
-        return 'Peixe';
-      case FoodType.Vegetariano:
-        return 'Vegetariano';
-      case FoodType.Dieta:
-        return 'Dieta';
-      case FoodType.Outro:
-        return 'Outro';
-      case FoodType.Bebida:
-        return 'Bebida';
-    }
+  else {
+    print(str);
     return null;
   }
+}
 
-  class Meal_ {
-  final FoodType foodType;
-  final String description;
-  bool isLunch;
-  double cost;
+String foodTypeToString(FoodType type) {
+  switch (type) {
+    case FoodType.Carne:
+      return 'Carne';
+    case FoodType.Peixe:
+      return 'Peixe';
+    case FoodType.Vegetariano:
+      return 'Vegetariano';
+    case FoodType.Dieta:
+      return 'Dieta';
+    case FoodType.Outro:
+      return 'Outro';
+    case FoodType.Bebida:
+      return 'Bebida';
+  }
+  return null;
+}
 
-  //final DateTime date;
-  //final Meal_ dayOfWeek; //??
+class Meal_ {
+final FoodType foodType;
+final String description;
+bool isLunch;
+double cost;
 
-  Meal_(this.foodType, this.description, this.isLunch, this.cost);
+//final DateTime date;
+//final Meal_ dayOfWeek; //??
+
+Meal_(this.foodType, this.description, this.isLunch, this.cost);
 
 /*
-  Map<String, dynamic> toMap(restaurantId) {
-    final DateFormat format = DateFormat('d-M-y');
-    return {
-      'id':null,
-      'day':toString(this.dayOfWeek),
-      'type':this.foodType,
-      'name':this.name,
-      'date': this.date != null ? format.format(this.date) : null,
-      'id_restaurant':restaurantId};
-  }
-  */
+Map<String, dynamic> toMap(restaurantId) {
+  final DateFormat format = DateFormat('d-M-y');
+  return {
+    'id':null,
+    'day':toString(this.dayOfWeek),
+    'type':this.foodType,
+    'name':this.name,
+    'date': this.date != null ? format.format(this.date) : null,
+    'id_restaurant':restaurantId};
+}
+*/
 
-  }
+}
